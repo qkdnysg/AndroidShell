dac971c325d2ca1f0093b9481fd22cde30e9dd6f
qkdnysg <qkdnysg@qq.com>
2016-09-21 10:35:03 +0800

    添加了启动第3个界面的点击事件

--- OriginalAPK/src/com/demo/originalapk/SecondActivity.java
+++ OriginalAPK/src/com/demo/originalapk/SecondActivity.java
@@ -1,10 +1,13 @@
 package com.demo.originalapk;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class SecondActivity extends Activity {
@@ -14,6 +17,12 @@ public class SecondActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		//setContentView(R.layout.activity_second);
 		TextView content = new TextView(this);
+		content.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+				startActivity(intent);
+			}});
 		content.setText("This is the second activity!");
 		
 		setContentView(content);
