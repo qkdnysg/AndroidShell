adde5a634f73923590fb2707b7839b9224a81e80
qkdnysg <qkdnysg@qq.com>
2016-12-30 09:27:25 +0800

    添加第四个Activity

--- OriginalAPK/src/com/demo/originalapk/ThirdActivity.java
+++ OriginalAPK/src/com/demo/originalapk/ThirdActivity.java
@@ -1,11 +1,13 @@
 package com.demo.originalapk;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -25,7 +27,7 @@ public class ThirdActivity extends Activity {
 		final TextView show = new TextView(this);
 		// 创建一个按钮
 		Button bn = new Button(this);
-		bn.setText("click me!");
+		bn.setText("获取当前时间");
 		bn.setLayoutParams(new ViewGroup.LayoutParams(
 				ViewGroup.LayoutParams.WRAP_CONTENT,
 				ViewGroup.LayoutParams.WRAP_CONTENT));
@@ -39,9 +41,22 @@ public class ThirdActivity extends Activity {
 			@Override
 			public void onClick(View v)
 			{
-				show.setText("Hello , Android , " + new java.util.Date());
+				show.setText("当前时间：" + new java.util.Date());
 			}
 		});
+		
+		Button btn = new Button(this);
+		btn.setText("启动第四个界面");
+		btn.setLayoutParams(new ViewGroup.LayoutParams(
+				ViewGroup.LayoutParams.WRAP_CONTENT,
+				ViewGroup.LayoutParams.WRAP_CONTENT));
+		layout.addView(btn);
+		btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(ThirdActivity.this, FourthMainActivity.class);
+				startActivity(intent);
+			}});
 	}
 
 }
